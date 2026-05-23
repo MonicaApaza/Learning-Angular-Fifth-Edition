01b6b0c7f4b096a5ab25d7f963a95d638b849fce
mapaza <monica.apazacora@gmail.com>
2026-05-23 11:52:38 -0400

    initial project

--- ch02/app.ts
+++ ch02/app.ts
@@ -1,1 +1,15 @@
-const title = 'Hello TypeScript!';
+const title = "hello ts";
+let isActive2 = true;
+console.log(isActive2);
+const categoriesDos = ["hola", "como", "estas"];
+console.log(categoriesDos);
+const adicional = ["el", "dia", 'de','hoy']
+const newAr = [...categoriesDos,...adicional]
+console.log(newAr); 
+
+type Categories = 'Computing' | 'multimedia';
+const categorie: Categories = "multimedia";
+
+function getProduct():string{
+    return "Computing";
+}
---
+++ ch02/app_orig.ts
@@ -0,0 +1,1 @@
+const titleTres = 'Hello TypeScript!';
